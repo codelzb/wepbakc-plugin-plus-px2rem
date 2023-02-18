b507bbd7bc7cb48717c732ceea95a40048a7b1a0
coderluo <zebluo@qq.com>
2023-02-19 00:49:34 +0800

    初版

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
-# wepbakc-plugin-plus-px2rem
-在公司中的老项目用到了wepbakc-plugin-px2rem loader, 这个loader原作者已经很久没有维护过了, 为了在老项目中修复原wepbakc-plugin-px2rem的BUG使用
+# 在公司中的老项目用到了[wepbakc-plugin-px2rem loader](https://www.npmjs.com/package/webpack-px-to-rem), 这个loader原作者已经很久没有维护过了, 为了在老项目中修复原插件的BUG使用
+
+复制了 CallMeXYZ 的插件，修复了包含 media query / calc(xxpx) /  fontSize  16/1.4px  的 bug  
+(media query 是不能改变的，如：@media (max-width:640px))
+
+## px2rem
+![npm](https://img.shields.io/npm/v/webpack-px2rem-loader.svg)
+![NPM](https://img.shields.io/npm/l/webpack-px2rem-loader.svg)
+![npm](https://img.shields.io/npm/dt/webpack-px2rem-loader.svg)
+
+开发响应式网页，尤其是手机端，常常采取rem单位。这个webpack的小loader主要是省去了每次输入长、宽、字体等都要把像素px换算成rem的麻烦，
+开发时直接输入px,最后打包打包时换算为rem。
+
+## 内容列表
+- [px2rem](#px2rem)
+- [内容列表](#内容列表)
+- [介绍](#介绍)
+- [安装](#安装)
+- [使用](#使用)
+- [原插件](#原插件)
+
+## 介绍
+这是你原先的css或者js代码
+```css
+// css
+div {
+    font-size: 14px;
+    width: 100px;
+}
+```
+```javascript
+// js 例如 react
+<Page style={{ fontSize: '14px', width: '100px' }} />
+```
+采取默认的 `1rem=10px` 转化后变成
+```css
+// css
+div {
+    font-size: 1.400rem;
+    width: 10rem;
+}
+```
+```javascript
+// js such as in react
+<Page style={{ fontSize: '1.400rem', width: '10rem' }} />
+```
+
+## 安装
+```javascript
+npm install webpack-px2rem-loader --save-dev
+```
+
+## 使用
+```javascript
+//in your webpack.config.js
+
+module.exports={
+   ...
+    module:{
+        // 或者 loaders
+        rules:[
+            {
+                test:/\.jsx$/,
+                loader:'webpack-px2rem-loader',
+                // 这个配置是可选的
+                 query:{
+                    // 1rem=npx 默认为 10
+                    basePx:10,
+                    // 只会转换大于min的px 默认为0
+                    // 因为很小的px（比如border的1px）转换为rem后在很小的设备上结果会小于1px，有的设备就会不显示
+                    min:1,
+                    // 转换后的rem值保留的小数点后位数 默认为3
+                    floatWidth:3
+                    // 排除要转换的px数值
+                    exclude:0,
+                }
+
+            }
+        ]
+    }
+  }
+```
---
+++ index.js
@@ -0,0 +1,51 @@
+var _ = require('lodash');
+var utils = require('loader-utils');
+const path = require('path')
+var isOpen = false
+var postcssConfig
+try{
+    let postcssPath = path.resolve(path.resolve(),'./postcss.config.js')
+    postcssConfig = require(postcssPath);
+}catch(err){
+    console.log('读取postcss.config.js时发生错误',err);
+}
+module.exports = function (source) {
+    if (_.isUndefined(this.cacheable)) return source;
+    this.cacheable();
+    var query = utils.parseQuery(this.query),
+        basePx = !_.isUndefined(query.basePx) ? query.basePx : 10,
+        min = !_.isUndefined(query.min) ? query.min : 0,
+        exclude = !_.isUndefined(query.exclude) ? query.exclude : null,
+        floatWidth = !_.isUndefined(query.floatWidth) ? query.floatWidth : 3,
+        matchPXExp = /([0-9,.]+px)([;| |}|'|"|);|/])/g;
+    
+    if(postcssConfig&&postcssConfig.plugins&&postcssConfig.plugins.length){
+        isOpen = postcssConfig.plugins.find(item => item.postcssPlugin===query.followPostcss)
+    }
+    if(query.followPostcss){
+        if(isOpen){
+            return source.replace(matchPXExp, function (match, m1, m2) {
+                var pxValue = parseFloat(m1.slice(0, m1.length - 2));
+                if (pxValue <= min) return match;
+                if (exclude&& pxValue == exclude) return match;
+                var remValue = pxValue / basePx;
+                if (pxValue % basePx != 0)
+                    remValue = (pxValue / basePx).toFixed(floatWidth);
+                return remValue + 'rem' + m2;
+            });
+        }else{
+            return source
+        }
+    }else{
+        return source.replace(matchPXExp, function (match, m1, m2) {
+            var pxValue = parseFloat(m1.slice(0, m1.length - 2));
+            if (pxValue <= min) return match;
+            if (exclude&& pxValue == exclude) return match;
+            var remValue = pxValue / basePx;
+            if (pxValue % basePx != 0)
+                remValue = (pxValue / basePx).toFixed(floatWidth);
+            return remValue + 'rem' + m2;
+        });
+    }
+};
+
